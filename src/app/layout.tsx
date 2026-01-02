e1040580da510fd26f78bea07403316844f46b3b
Muhammad Zhifrantino <herrmadz@gmail.com>
2026-01-03 02:18:48 +0700

    gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,10 +65,11 @@ export default function RootLayout({
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-            gtag('config', 'G-Z607BFHM0N');
+  gtag('config', 'G-Z607BFHM0N');
+
           `}
         </Script>
 
