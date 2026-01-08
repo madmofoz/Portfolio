ebf19636eb895c7382ca699cadabc96fc00c3fe4
Muhammad Zhifrantino <herrmadz@gmail.com>
2026-01-08 20:49:29 +0700

    robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://portolio-beta-black.vercel.app/sitemap.xml',
+  }
+}
