97c69352a74903192b0718ea69d03c624a4e3260
Muhammad Zhifrantino <herrmadz@gmail.com>
2026-01-12 06:12:28 +0700

    fix sample client component

--- src/app/projects/sample/page.tsx
+++ src/app/projects/sample/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState, useEffect, useRef } from 'react';
 import { motion, useSpring } from 'framer-motion';
 import { Mail, Lock, Eye, EyeOff, Github, Chrome } from 'lucide-react';
