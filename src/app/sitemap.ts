ba240b0cb38e6b3077f474135dff33ee07741904
Muhammad Zhifrantino <herrmadz@gmail.com>
2026-03-01 04:02:50 +0700

    sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,34 +11,40 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: `${baseUrl}/siliminpro`,
+      url: `${baseUrl}/projects/siliminpro`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
     },
     {
-      url: `${baseUrl}/khad-team`,
+      url: `${baseUrl}/projects/khad-team`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/silimin`,
+      url: `${baseUrl}/projects/silimin`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/kawa-kawa`,
+      url: `${baseUrl}/projects/kawa-kawa`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/sample`,
+      url: `${baseUrl}/projects/sample`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.8,
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/projects/certificates`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
     },
   ]
 }
