989c72c2387ebf4bd2213ac6f4fa794beb69593f
Muhammad Zhifrantino <herrmadz@gmail.com>
2026-04-15 07:24:52 +0700

    gsc anj

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://portolio-beta-black.vercel.app'
+  const baseUrl = 'https://portfolio-zhifrantino.vercel.app'
 
   return [
     {
